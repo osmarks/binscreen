c54d213cb4d9c7dce4a5fd141fb21f25473e3661
Oliver Marks <osmarks@icloud.com>
2016-12-26 17:03:08 +0000

    Tidying up
    Clean up indenting, add more comments & correct the actually_run in line 20 to actuallyRun.

--- binscreen.lua
+++ binscreen.lua
@@ -1,29 +1,30 @@
 local actuallyRun = true
 
 function start(feed)
-  local worldwideEHyperWeb = require "internet"
-  local term = require "term"
-  local component = require "component"  
+	local worldwideEHyperWeb = require "internet"
+	local term = require "term"
+	local component = require "component"  
+	--Get necessary libraries.
 
-  while actuallyRun do
-	component.gpu.setResolution(60, 30)
-	--That sets the resolution. For a square screen, the second number must be half the first for some reason.
-	  term.clear()
-    local request = worldwideEHyperWeb.request(feed)
-    local data = ""
-    
-    for chunk in request do
-      data = data .. chunk
-    end
+	while actuallyRun do --To allow dry-runs of the system.
+		component.gpu.setResolution(60, 30)
+		--That sets the resolution. For a square screen, the second number must be half the first for some reason
+		term.clear() --Wipe previous text off screen.
+		local request = worldwideEHyperWeb.request(feed) --Download the feed data
+		local data = ""
+  
+		for chunk in request do --OC's internet requests are for some reason done in chunks.
+			data = data .. chunk
+		end
 
-    if data == "STOP" then
-      actually_run = false
-    else
-      term.write(data, true)
-    end
-    
-    os.sleep(60)
-  end
+		if data == "STOP" then
+			actuallyRun = false --Stop, so that it's possible to modify configs on already-running screen setups.
+		else
+			term.write(data, true)
+		end
+		
+		os.sleep(60) --Wait before redownloading everything
+	end
 end
 
 --start("YOUR_ADDRESS_HERE") --If launching without using rc.d then uncomment & swap this out for your URL.
