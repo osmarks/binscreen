dbc510c3e0c06d2f846e050d114c6d76e62aed25
Oliver Marks <osmarks@icloud.com>
2016-12-27 14:07:11 +0000

    Refactoring
    Split into functions, and also keep all the chars inside a char array (technically a table).

--- binscreen.lua
+++ binscreen.lua
@@ -1,32 +1,57 @@
 local actuallyRun = true
 
-function start(feed)
-	local worldwideEHyperWeb = require "internet"
-	local term = require "term"
-	local component = require "component"  
-	--Require the OpenOS libraries necessary for operation.
-
-	while actuallyRun do --To allow dry-runs of the system.
-		component.gpu.setResolution(60, 30)
-		--That sets the resolution. For a square screen, the second number must be half the first for some reason
-		term.clear() --Wipe previous text off screen.
-		local request = worldwideEHyperWeb.request(feed) --Download the feed data
-		local data = ""
+local function netRequest(url)
+	local web = require "internet"
+	local request = web.request(url) --Download the feed data
+	local data = ""
   
-		for chunk in request do --OC's internet requests are for some reason done in chunks.
-			data = data .. chunk
-		end
+	for chunk in request do --OC's internet requests are for some reason done in chunks.
+		data = data .. chunk
+	end
+	
+	return data
+end
 
-		if data == "STOP" then
-			actuallyRun = false --Stop, so that it's possible to modify configs on already-running screen setups.
-		else
-			data:gsub(".", function(char)
-					term.write(char, true) --Write each character of `data to screen, and wrap text. 
-			end)
-		end
+local function stringToChars(str)
+	chars = {}
+	
+	str:gsub(".", function(char)
+		table.insert(chars, char) --Insert each char into an array
+	end)
+	
+	return chars
+end
+
+local function runOnce(feed)
+	local component = require "component"
+	local term = require "term"
+	--component and term are only necessary here, so require them here.
+	
+	component.gpu.setResolution(60, 30)
+	--Set resolution. For a square screen, the second number must be half the first for some reason
+	term.clear() --Wipe previous text off screen.
+
+	local data = netRequest(feed)
+	
+	if data == "STOP" then
+		actuallyRun = false --Stop, so that it's possible to modify configs on already-running screen setups.
+	else
+		local chars = stringToChars(data)
 		
-		os.sleep(60) --Wait before redownloading & redrawing everything
+		for index, char in ipairs(chars) do
+			term.write(char, true) --Print out char array
+		end
 	end
 end
 
---start("YOUR_ADDRESS_HERE") --If launching without using rc.d then uncomment & swap this out for your URL.
+function start(feed)
+	local hasRun = false
+
+	while actuallyRun or not hasRun do
+		runOnce(feed)
+		hasRun = true
+		os.sleep(60) --Wait before redownloading & redrawing everything	
+	end
+end
+
+start("YOUR_ADDRESS_HERE") --Your URL here, even if using rc for some reason
