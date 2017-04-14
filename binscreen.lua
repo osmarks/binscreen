94c16271ff82d448dcbeaf5baaea94c0ee3451d6
Oliver Marks <osmarks@icloud.com>
2017-04-14 21:45:35 +0100

    Move stuff to constants at top
    The refresh time, horizontal resolution, vertical resolution and data URL have been moved to easily-accessible constants at the top.

--- binscreen.lua
+++ binscreen.lua
@@ -1,22 +1,25 @@
-local actuallyRun = true
+local DATA_URL = "http://your.url/here"
+local RESOLUTION_HORIZONTAL, RESOLUTION_VERTICAL = 60, 30
+local REFRESH_TIME = 60
+local continueRunning = true -- Turn off for a test run (will exit after one load)
 
 local function netRequest(url)
 	local web = require "internet"
-	local request = web.request(url) --Download the feed data
+	local request = web.request(url) -- Download the feed data
 	local data = ""
   
-	for chunk in request do --OC's internet requests are for some reason done in chunks.
+	for chunk in request do -- OC's internet requests are for some reason done in chunks.
 		data = data .. chunk
 	end
 	
-	return data:gsub("\r", "")
+	return data:gsub("\r", "") -- Strip out \r
 end
 
 local function stringToChars(str)
 	chars = {}
 	
 	str:gsub(".", function(char)
-		table.insert(chars, char) --Insert each char into an array
+		table.insert(chars, char)
 	end)
 	
 	return chars
@@ -25,33 +28,32 @@ end
 local function runOnce(feed)
 	local component = require "component"
 	local term = require "term"
-	--component and term are only necessary here, so require them here.
 	
-	component.gpu.setResolution(60, 30)
-	--Set resolution. For a square screen, the second number must be half the first for some reason
-	term.clear() --Wipe previous text off screen.
+	component.gpu.setResolution(RESOLUTION_HORIZONTAL, RESOLUTION_VERTICAL)
+	-- Set resolution. For a square screen, the second number must be half the first for some reason
+	term.clear() -- Wipe previous text off screen.
 
 	local data = netRequest(feed)
 	
 	if data == "STOP" then
-		actuallyRun = false --Stop, so that it's possible to modify configs on already-running screen setups.
+		continueRunning = false -- Stop on certain message to allow config modifications on running screen setups.
 	else
 		local chars = stringToChars(data)
 		
 		for index, char in ipairs(chars) do
-			term.write(char, true) --Print out char array
+			term.write(char, true) -- Print out char array & wrap.
 		end
 	end
 end
 
 function start(feed)
-	local hasRun = false
+	local runs = 0
 
-	while actuallyRun or not hasRun do
+	while continueRunning or runs < 1 do -- Program should run at least once.
 		runOnce(feed)
-		hasRun = true
-		os.sleep(60) --Wait before redownloading & redrawing everything	
+		runs = runs + 1
+		os.sleep(REFRESH_TIME) -- Wait before redownloading & redrawing everything	
 	end
 end
 
-start("YOUR_ADDRESS_HERE") --Your URL here, even if using rc for some reason
+start(DATA_URL)
