7c3ec708ccd621d5dbb7e8f2b7ffa8d6c317e69c
Oliver Marks <osmarks@icloud.com>
2017-06-25 18:37:51 +0100

    Add a config file (no actual config options yet)

--- binscreen.lua
+++ binscreen.lua
@@ -1,4 +1,5 @@
 local DATA_URL = "http://your.url/here"
+local CFG_URL = "http://your.other/url/here"
 local RESOLUTION_HORIZONTAL, RESOLUTION_VERTICAL = 60, 30
 local REFRESH_TIME = 60
 local continueRunning = true -- Turn off for a test run (will exit after one load)
@@ -15,38 +16,26 @@ local function netRequest(url)
 	return data:gsub("\r", "") -- Strip out \r
 end
 
-local function stringToChars(str)
-	chars = {}
-	
-	str:gsub(".", function(char)
-		table.insert(chars, char)
-	end)
-	
-	return chars
-end
-
-local function runOnce(feed)
+local function runOnce(dataURL, cfgURL)
 	local component = require "component"
 	local term = require "term"
+	local serialization = require "serialization"
 	
 	component.gpu.setResolution(RESOLUTION_HORIZONTAL, RESOLUTION_VERTICAL)
 	-- Set resolution. For a square screen, the second number must be half the first for some reason
 	term.clear() -- Wipe previous text off screen.
 
-	local data = netRequest(feed)
+	local data = netRequest(dataURL)
+	local cfg = serialization.unserialize(cfgURL)
 	
 	if data == "STOP" then
 		continueRunning = false -- Stop on certain message to allow config modifications on running screen setups.
 	else
-		local chars = stringToChars(data)
-		
-		for index, char in ipairs(chars) do
-			term.write(char, true) -- Print out char array & wrap.
-		end
+		print(data)
 	end
 end
 
-function start(feed)
+function start(data, cfg)
 	local runs = 0
 
 	while continueRunning or runs < 1 do -- Program should run at least once.
@@ -56,4 +45,4 @@ function start(feed)
 	end
 end
 
-start(DATA_URL)
+start(DATA_URL, CFG_URL)
